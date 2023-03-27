f25fd1126ab6a67dad8d2134db5c71344739204d
Isabel <isabelgalwey@gmail.com>
2023-03-27 15:58:16 +0100

    questions 1-9 and 1-4

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -13,17 +13,30 @@ public class Runner {
         scottishIslands.add("Skye");
         scottishIslands.add("Arran");
         scottishIslands.add("Tresco");
+        scottishIslands.add("Coll");
+        scottishIslands.add(0, "Tiree");
+        scottishIslands.add(2,"Islay");
+        int island = scottishIslands.indexOf("Skye");
+        System.out.println(island);
+        scottishIslands.remove("Tresco");
+        System.out.println(scottishIslands);
+        scottishIslands.remove(5);
+        System.out.println(scottishIslands.size());
+        Collections.sort(scottishIslands);
+        for (String islandsList : scottishIslands){
+            System.out.println(islandsList);
+        }
 
 
-//        1. Add "Coll" to the end of the list
-//        2. Add "Tiree" to the start of the list
-//        3. Add "Islay" after "Jura" and before "Mull"
-//        4. Print out the index position of "Skye"
-//        5. Remove "Tresco" from the list by name
-//        6. Remove "Arran" from the list by index
-//        7. Print the number of islands in your arraylist
-//        8. Sort the list alphabetically
-//        9. Print out all the islands using a for loop
+//        1. Add "Coll" to the end of the list X
+//        2. Add "Tiree" to the start of the list X
+//        3. Add "Islay" after "Jura" and before "Mull" X
+//        4. Print out the index position of "Skye" X
+//        5. Remove "Tresco" from the list by name X
+//        6. Remove "Arran" from the list by index X
+//        7. Print the number of islands in your arraylist X
+//        8. Sort the list alphabetically X
+//        9. Print out all the islands using a for loop X
 
         System.out.println(scottishIslands);
 
@@ -33,9 +46,45 @@ public class Runner {
 
         System.out.println("numbers: " + numbers);
 
-//        1. Print out a list of the even integers
-//        2. Print the difference between the largest and smallest value
-//        3. Print True if the list contains a 1 next to a 1 somewhere.
+//        for (int evens : numbers) {
+//            if (evens %2 == 0){
+//            System.out.println(evens);
+//             }
+//        }
+        // 1. create for loops to find biggest and smallest numbers, create variables
+        // 2. print 'biggest' - 'smallest' variables
+        int maxNumber = Collections.max(numbers);
+        int minNumber = Collections.min(numbers);
+        System.out.println(maxNumber - minNumber);
+
+
+
+// if index = index +/- 1 AND value = 1,  print TRUE
+// if number.get(i) = value && i == 1 print i value, i = value
+
+       boolean ones  = false;
+        int value = 0;
+        for (int i = 0; i < numbers.size(); i++){
+            if  (numbers.get(i) == value && numbers.get(i) == 1){
+                ones = true;
+                System.out.println(ones);
+
+            } else{
+                value = numbers.get(i);
+            }
+
+        }
+
+
+        int total = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+             total = numbers.get(i) + total;
+        }
+        System.out.println(total);
+
+//        1. Print out a list of the even integers X
+//        2. Print the difference between the largest and smallest value X
+//        3. Print True if the list contains a 1 next to a 1 somewhere. X
 //        4. Print the sum of the numbers,
 //        5. Print the sum of the numbers...
 //           ...except the number 13 is unlucky, so it does not count...
